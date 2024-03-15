efe08c2ff1e92df1a1f1a1cc9e048a27207b5de4
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 01:34:37 -0400

    Created home screen
    Created home screen

---
+++ src/page/Home.tsx
@@ -0,0 +1,19 @@
+// Style imports
+import "../style/page/home.css"
+
+const Home = () => {
+    return (
+        <>
+            <div className="body-content"> 
+                <figure className="hero-content rounded text-center">
+                    <h1 style={{fontSize: "xx-large"}}>Money Market</h1>
+                    <h3 style={{fontSize: "x-large"}}>Alpha Storm Project</h3>
+                </figure>
+            </div>
+        </>
+    );
+}
+
+export default Home;
+
+                    
