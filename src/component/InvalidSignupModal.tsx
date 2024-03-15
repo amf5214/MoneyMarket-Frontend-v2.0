378432e0e19e91cbfb47ef9cf297ca1d51aa2fd8
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 02:05:35 -0400

    Created modal for invalid sign up
    Created modal for invalid sign up

---
+++ src/component/InvalidSignupModal.tsx
@@ -0,0 +1,36 @@
+import { Divider, Modal, ModalBody, ModalContent, ModalHeader, ModalFooter, Button } from "@nextui-org/react"
+
+interface Props {
+    setShow: (show:boolean) => void;
+    show: boolean;
+}
+
+export const InvalidSignupModal = ( { show, setShow }:Props ) => {
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    return (
+        <Modal 
+            size={"md"} 
+            isOpen={show} 
+            onClose={handleClose}
+            backdrop="blur"
+            classNames={{base:"", header:"border-b-[1px]", footer: "border-t-[1px]", backdrop: "bg-red-500"}}
+            >
+            <ModalContent className="">
+                <ModalHeader className="flex flex-col gap-1">Invalid Sign Up</ModalHeader>
+                <ModalBody>
+                    <p>The credentials provided are already taken</p>
+                </ModalBody>
+                <ModalFooter>
+                    <Button variant="shadow" color="primary" onPress={handleClose}>
+                    Close
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>    
+    )
+    
+}
