2e9de9136e5d4aa4dc6c1c456fb06a3333f0916f
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 01:35:04 -0400

    Added paths service and created home page routing
    Added paths service and created home page routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,17 @@ import {
 // Style imports 
 import './style/App.css';
 
+// Local imports
+import Paths from "./services/paths.service";
+import Home from "./page/Home";
 import Toolbar from './component/Toolbar';
 function App() {
 
   return (
       <Router>
         <Toolbar />
+        <Routes>
+          <Route path={Paths.HOME} element={<Home />} />
       </Router>
   )
 }
---
+++ src/services/paths.service.ts
@@ -0,0 +1,7 @@
+enum Paths {
+    HOME = "/",
+    SIGNIN = "/signin",
+    API_BASE = "http://localhost:5000"
+}
+
+export default Paths;
