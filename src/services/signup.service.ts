45b7ce7677d369eae460b8b1b96012e92cba1b7c
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 02:06:18 -0400

    Created signup service logic
    Created signup service logic

---
+++ src/services/signup.service.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import Paths from "./paths.service";
+
+export const handleSignUp = async (firstName:string, lastName:string, email:string, password:string, username:string) => {
+    try {
+        const response = await axios.post(`${Paths.API_BASE}/auth/signin`,
+        JSON.stringify({
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            password:password,
+            username:username,
+        }),
+        {
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        return await response.data['access_token'];
+
+    } catch(err) {
+        console.log(err)
+        return null;
+    }
+}
