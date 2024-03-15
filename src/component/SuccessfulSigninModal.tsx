ef8656c4a612aa1e8719a4de83473613733a60e6
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 01:35:34 -0400

    Created modal to show upon successful sign in
    Created modal to show upon successful sign in

---
+++ src/component/SuccessfulSigninModal.tsx
@@ -0,0 +1,36 @@
+import { Divider, Modal, ModalBody, ModalContent, ModalHeader, ModalFooter, Button } from "@nextui-org/react"
+
+interface Props {
+    setShow: (show:boolean) => void;
+    show: boolean;
+}
+
+export const SuccessfulSigninModal = ( { show, setShow }:Props ) => {
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    return (
+        <Modal 
+            size={"md"} 
+            isOpen={show} 
+            onClose={handleClose}
+            backdrop="blur"
+            classNames={{base:"", header:"border-b-[1px]", footer: "border-t-[1px]", backdrop: "bg-green-600"}}
+            >
+            <ModalContent className="">
+                <ModalHeader className="flex flex-col gap-1">Success</ModalHeader>
+                <ModalBody>
+                    <p>Successful Sign In</p>
+                </ModalBody>
+                <ModalFooter>
+                    <Button variant="shadow" color="primary" onPress={handleClose}>
+                    Close
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>    
+    )
+    
+}
