b2849f0a190f07d87eb6fb244aa8c5362d295e89
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 14:21:44 -0400

    Added additional paths
    Added additional paths

--- src/services/paths.service.ts
+++ src/services/paths.service.ts
@@ -1,8 +1,9 @@
 enum Paths {
-    HOME = "/",
-    SIGNIN = "/signin",
-    MARKET_NEWS = "/market-news",
-    API_BASE = "http://localhost:5000"
+  HOME = "/",
+  SIGNIN = "/signin",
+  MARKET_NEWS = "/market-news",
+  API_BASE = "http://localhost:5000",
+  LIVE_MARKETS = "/live-markets"
 }
 
 export default Paths;
