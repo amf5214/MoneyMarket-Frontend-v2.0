1772ac2a30ac28b473ea4505a70c8021a1ff78b6
amf5214 <austinfraley.lee@gmail.com>
2024-03-15 01:32:22 -0400

    Created widescreen toolbar elements
    Created widescreen toolbar elements

--- src/component/Toolbar.tsx
+++ src/component/Toolbar.tsx
@@ -1,5 +1,13 @@
 // React imports
 import { useState } from "react";
+
+// NextUI imports
+import { Avatar, Button, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, User } from "@nextui-org/react";
+
+// Other imports
+import SearchIcon from '@mui/icons-material/Search';
+
+// Style imports
 import '../style/component/toolbar.css'
 
 const Toolbar = () => {
@@ -8,6 +16,30 @@ const Toolbar = () => {
 
     return (
         <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="full" isBordered className="h-full relative flex navheader">
+            {/* Logo and company name */ }
+            <NavbarContent className="sm" justify="start">
+                <NavbarBrand>
+                    <Link href="/" color={"foreground"}>
+                        <Avatar src="logo.jpg" className="rounded" style={{marginRight: ".5rem"}}/>
+                        <p style={{ marginLeft: ".5rem" }}>Money Market</p>
+                    </Link>  
+                </NavbarBrand>
+                 {/* Nav buttons in the middle of the screen - Visibility = Wide screen */ }
+                 <NavbarContent className="hidden lg:flex gap-4" justify="center">
+                    <Link href="#" color={"foreground"}>
+                        Live Markets
+                    </Link>
+                    <Link href="#" color={"foreground"}>
+                        Market News
+                    </Link>
+                    <Link href="#" color={"foreground"}>
+                        Learning Hub
+                    </Link>
+                    <Link href="#" color={"foreground"}>
+                        Content Hub
+                    </Link>
+                </NavbarContent>
+            </NavbarContent>
         </Navbar>
     )
 }
