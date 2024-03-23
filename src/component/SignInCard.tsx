46a89922e7559cbea8c01a48159e2f1af6d02b2a
amf5214 <81787764+amf5214@users.noreply.github.com>
2024-03-23 02:19:12 -0400

    Reformatted sign in card
    Reformatted sign in card

--- src/component/SignInCard.tsx
+++ src/component/SignInCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardBody, Divider } from "@nextui-org/react";
+import { Button, Card, CardBody, Divider, Input, Textarea } from "@nextui-org/react";
 import { handleSignin } from "../services/auth/signin.service";
 import { useContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
@@ -47,19 +47,16 @@ export const SignInCard = () => {
         <>        
             <InvalidSigninModal show={showInvalid} setShow={setShowInvalid} />
             <SuccessfulSigninModal show={showSuccess} setShow={hideSuccess} />
-            <Card className="min-w-[25vw] min-h-[50vh]">
+            <Card className="min-w-[25vw] min-h-[50vh]" style={{filter: "drop-shadow(10px 10px 10px)", padding: "2rem"}}>
                 <CardBody className="flex flex-col justify-around items-center">
                     <h1 className="text-center font-bold text-slate-800" style={{fontSize: "x-large", fontWeight: "bold"}}>Sign In</h1>
                     <form className="flex flex-col gap-y-4 justify-around items-center"
-                        style={{flex: ".5 0 auto", lineHeight: "2rem", marginBottom: "1rem"}}>
-                        <figure style={{ marginBottom: "0.5rem" }}>
-                            <h1 className="text-center">Email address:</h1>
-                            <input autoFocus type="email" value={email} placeholder="Enter email" className="text-center rounded-lg bg-slate-800 text-white" onChange={(e) => setEmail(e.target.value)}/>
+                        style={{flex: ".5 0 auto", lineHeight: "2rem", marginBottom: "1rem", backgroundColor: "white"}}>
+                        <figure className="rounded-lg" style={{ marginBottom: "0.5rem", padding: "1rem"}}>
+                            <Input variant="underlined" label="Email Address:" labelPlacement="outside" autoFocus type="email" value={email} placeholder="Enter email" className="rounded-lg " onChange={(e) => setEmail(e.target.value)}/>
                         </figure>
-                        <Divider />
-                        <figure >
-                            <h1 className="text-center">Password:</h1>
-                            <input type="password" value={pwd} placeholder="Enter password" className="text-center rounded-lg bg-slate-800 text-white" onChange={(e) => setPwd(e.target.value)}/>
+                        <figure className="rounded-lg" style={{ padding: "1rem"}}>
+                            <Input variant="underlined" label="Password:" labelPlacement="outside" type="password" value={pwd} placeholder="Enter password" className="rounded-lg " onChange={(e) => setPwd(e.target.value)}/>
                         </figure>
                     </form>
                     <Button color="primary" variant="shadow" className="max-w-[100%]" onClick={handleSubmit}>
