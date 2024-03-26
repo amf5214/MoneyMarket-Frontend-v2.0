037be30065f896cb3d3970b880d47c2de81c6603
amf5214 <austinfraley.lee@gmail.com>
2024-03-26 18:28:08 -0400

    Redesigned market news page
    Redesigned market news page

--- src/page/MarketNews.tsx
+++ src/page/MarketNews.tsx
@@ -3,10 +3,12 @@ import Cookies from "js-cookie";
 import getNews from "../services/market-news/marketnews.service";
 import { NewsStory } from "../services/market-news/newsstory.dto";
 import AuthContext from "../provider/AuthProvider";
-import { useNavigate } from "react-router-dom";
+import { redirect, useHref, useNavigate } from "react-router-dom";
 import { UndoRounded } from "@mui/icons-material";
 import { ApiError } from "../services/error.service";
 import Toolbar from "../component/Toolbar";
+import "../style/page/marketnews.css"
+import { Card, CardBody, CardFooter, CardHeader, Divider, Image } from "@nextui-org/react";
 
 export const MarketNewsPage = () => {
 
@@ -39,24 +41,33 @@ export const MarketNewsPage = () => {
             <div className="bg-gray-100 market-news-body">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none">
-                        <h2 className="text-2xl font-bold text-gray-900">News</h2>
-                        <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-                            {news.slice(0, 3).map((story) => (
-                                <div className="group relative">
-                                    <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                                        <img src={story.image_url} alt={story.title} className="h-full w-full object-cover object-center" />
-                                    </div>
-                                    <h3 className="mt-6 text-sm text-gray-500">
-                                        <a href={story.url} >
-                                        <span className="absolute inset-0"></span>
-                                        {story.title}
-                                        </a>
-                                    </h3>
-                                    <p className="text-base font-semibold text-gray-900">{story.description}</p>
-                                </div> 
-                            ))}
-                            
-                        </div>
+                        <Card className="bg-slate-800">
+                            <CardHeader className="flex justify-center bg-slate-900">
+                                <h2 className="text-2xl font-bold text-white text-center">News</h2>
+                            </CardHeader>
+                            <Divider className="bg-gray-500" />
+                            <CardBody>
+                                <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 bg-slate-800 p-4 rounded-lg">
+                                    {news.slice(0, 3).map((story, ind) => (
+                
+                                        <Card shadow="sm" key={ind} isPressable onPress={() => window.location.href = story.url}>
+                                            <CardBody className="overflow-visible p-0">
+                                                <Image
+                                                    shadow="sm"
+                                                    radius="lg"
+                                                    alt={story.title}
+                                                    className="w-full object-cover h-[50vh] w-[100vw] lg:w-[35vw]"
+                                                    src={story.image_url}
+                                                />
+                                            </CardBody>
+                                            <CardFooter className="text-small justify-between">
+                                                <b>{story.title}</b>
+                                            </CardFooter>
+                                        </Card>
+                                    ))}   
+                                </div>
+                            </CardBody>
+                        </Card>
                     </div>
                 </div>
             </div>
