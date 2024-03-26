bd2dca6292a9fa3bdb40fe58b555a53b65249098
amf5214 <austinfraley.lee@gmail.com>
2024-03-26 17:20:27 -0400

    Separated base path live markets
    Separated base path live markets to render live markets home

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import SignInPage from "./page/SignIn";
 import { MarketNewsPage } from "./page/MarketNews";
 import { LiveMarketsPage } from "./page/LiveMarkets";
 import PrivateRoutes from "./component/PrivateRoutes";
+import { LiveMarketsHomePage } from "./page/LiveMarketsHome";
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
           <Route element={<PrivateRoutes />}>
             <Route path={Path.MARKET_NEWS} element={<MarketNewsPage />} />
             <Route path={Path.LIVE_MARKETS + "/:ticker"} element={<LiveMarketsPage />} />
+            <Route path={Path.LIVE_MARKETS} element={<LiveMarketsHomePage />} />
           </Route>
         </Routes>
       </Router>
