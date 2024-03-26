2d0bd50577a6b4331c2f67e8fff18a2244eb1647
amf5214 <austinfraley.lee@gmail.com>
2024-03-25 23:04:37 -0400

    Added live markets path parameter
    Added live markets path parameter

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           <Route path={Path.SIGNIN} element={<SignInPage />} />
           <Route element={<PrivateRoutes />}>
             <Route path={Path.MARKET_NEWS} element={<MarketNewsPage />} />
-            <Route path={Path.LIVE_MARKETS} element={<LiveMarketsPage />} />
+            <Route path={Path.LIVE_MARKETS + "/:ticker"} element={<LiveMarketsPage />} />
           </Route>
         </Routes>
       </Router>
