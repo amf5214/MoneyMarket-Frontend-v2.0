34f0546742b823c0957a04fbac51ebb8f9c87036
amf5214 <austinfraley.lee@gmail.com>
2024-03-29 10:10:11 -0400

    Added paths for new pages
    Added paths for new pages

--- src/services/path.service.ts
+++ src/services/path.service.ts
@@ -1,12 +1,16 @@
 enum Path {
+    API_BASE = "http://localhost:5000",
     HOME = "/",
     SIGNIN = "/signin",
+    ACCOUNT = "/account",
+    SETTINGS = "/settings",
     MARKET_NEWS = "/market-news",
-    API_BASE = "http://localhost:5000",
     LIVE_MARKETS = "/live-markets",
+    LIVE_MARKETS_WATCHLIST = "/live-markets/watchlist",
     CONTENT_HUB = "/content-hub",
     LEARNING_HUB = "/learning-hub",
     LEARNING_SERIES = "/learning-hub/series",
+    LEARNING_HUB_COURSE_BOOKMARKS = "/learning-hub/bookmarks"
 }
 
 export default Path;
