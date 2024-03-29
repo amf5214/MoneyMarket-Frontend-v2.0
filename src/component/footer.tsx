7b88b9087f7b54b35da20243e6d582eb02cab634
amf5214 <austinfraley.lee@gmail.com>
2024-03-29 10:15:56 -0400

    Reformatted footer and updated file naming convention
    Reformatted footer and updated file naming convention

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -4,17 +4,17 @@ export const Footer = () => {
     return (
         <>
             <div className="w-full h-1/4 bg-black p-4 flex justify-around">
-               <div className="flex flex-col m-3">
+               <div className="flex flex-col">
                     <p className="text-white text-small text-center hover:underline"><a href={Path.HOME}>Home</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.ACCOUNT}>Account</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.SETTINGS}>Settings</a></p>
                </div>
-               <div className="flex flex-col m-3">
+               <div className="flex flex-col">
                     <p className="text-white text-small text-center hover:underline"><a href={Path.LIVE_MARKETS}>Live Markets Home</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.LIVE_MARKETS_WATCHLIST}>Watch List</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.MARKET_NEWS}>Market News</a></p>
                </div>
-               <div className="flex flex-col m-3">
+               <div className="flex flex-col">
                     <p className="text-white text-small text-center hover:underline"><a href={Path.LEARNING_HUB}>Learning Hub</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.CONTENT_HUB}>Content Hub</a></p>
                     <p className="text-white text-small text-center hover:underline"><a href={Path.LEARNING_HUB_COURSE_BOOKMARKS}>Course Bookmarks</a></p>
