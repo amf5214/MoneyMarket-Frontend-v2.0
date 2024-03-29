a131449538d22634d732de7bfc402daded48980a
amf5214 <austinfraley.lee@gmail.com>
2024-03-29 08:50:58 -0400

    Created paths for learning pages
    Created paths for learning pages

--- src/services/path.service.ts
+++ src/services/path.service.ts
@@ -5,7 +5,8 @@ enum Path {
     API_BASE = "http://localhost:5000",
     LIVE_MARKETS = "/live-markets",
     CONTENT_HUB = "/content-hub",
-    LEARNING_HUB = "/learning-hub"
+    LEARNING_HUB = "/learning-hub",
+    LEARNING_SERIES = "/learning-hub/series",
 }
 
 export default Path;
