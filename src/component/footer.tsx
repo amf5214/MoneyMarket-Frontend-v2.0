a9db09b9f518b978f3c556da3f2e4797a29980c7
amf5214 <austinfraley.lee@gmail.com>
2024-03-29 11:40:17 -0400

    Removed Footer content
    Removed Footer content

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -3,6 +3,7 @@ import Path from "../services/path.service";
 import "../style/component/footer.css";
 import { useState } from "react";
 import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 export const Footer = () => {
@@ -11,34 +12,16 @@ export const Footer = () => {
         <>
         {showFooter == false ?
             <div className="w-full h-1/4 footer-wrapper">
-                <Button className="w-full bg-slate-900 text-gray-500" onClick={e => setShowFooter(true)}>
-                    Footer
+                <Button className="w-full text-black" onClick={e => setShowFooter(true)}>
+                    <MenuIcon />
                 </Button>
             </div>
         :
-            <div className="w-full h-1/4 footer-wrapper bg-transparent">
-                <div className="w-full h-1/4 bg-slate-900 flex flex-col content-center items-center justify-center p-4">
+            <div className="w-full h-1/2 footer-wrapper">
+                <div className="w-full flex flex-col content-center items-center justify-center p-4">
                     <Button className="bg-transparent text-white" onClick={e => setShowFooter(false)} style={{alignSelf: "flex-start"}}>
                         <CloseIcon />
-                    </Button>
-                    <h1 className="text-md text-white underline">Links</h1>
-                    <div className="w-full p-4 bg-transparent flex justify-around">
-                        <div className="flex flex-col">
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.HOME}>Home</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.ACCOUNT}>Account</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.SETTINGS}>Settings</Link>
-                        </div>
-                        <div className="flex flex-col">
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.LIVE_MARKETS}>Live Markets Home</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.LIVE_MARKETS_WATCHLIST}>Watch List</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.MARKET_NEWS}>Market News</Link>
-                        </div>
-                        <div className="flex flex-col">
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.LEARNING_HUB}>Learning Hub</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.CONTENT_HUB}>Content Hub</Link>
-                                <Link className="text-small hover:underline text-white hover:text-white" href={Path.LEARNING_HUB_COURSE_BOOKMARKS}>Course Bookmarks</Link>
-                        </div>
-                    </div>
+                    </Button>                
                 </div>
             </div>
         }
